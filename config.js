9dbc380234f8dd91ec21d90d042e8fd0cbfeb0a6
Nadeenpoorna-lkz <nadeepoorna987@gmail.com>
2025-02-02 16:21:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,6 +9,9 @@ SESSION_ID: process.env.SESSION_ID || "", //put your session id
 MONGODB: process.env.MONGODB || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority",
 MODE: process.env.MODE || "private", //private | public | groups | inbox
 ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true", //true or false
+STATUES_REPLY: process.env.STATUES_REPLY || "true", //true or false
+AUTO_REACT: process.env.AUTO_REACT || "false", //true or false
+INBOX_BLOCK: process.env.INBOX_BLOCK || "false", //true or false
 AUTO_TYPING: process.env.AUTO_TYPING || "false", //true or false
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", //true or false
 AUTO_VOICE:"true" //true or false
