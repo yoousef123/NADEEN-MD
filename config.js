31e47ac0ed96bc3b8f1d1187108da30f50ef3187
Nadeenpoorna-lkz <nadeepoorna987@gmail.com>
2025-02-07 22:33:27 +0530

    👨🏻‍💻NADEEN-MD👨🏻‍💻

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ MODE: process.env.MODE || "private", //private | public | groups | inbox
 ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true", //true or false
 STATUES_REPLY: process.env.STATUES_REPLY || "true", //true or false
 AUTO_REACT: process.env.AUTO_REACT || "false", //true or false
-INBOX_BLOCK: process.env.INBOX_BLOCK || "false", //true or false
+INBOX_BLOCK: process.env.INBOX_BLOCK || "false", //true or false 
 AUTO_TYPING: process.env.AUTO_TYPING || "false", //true or false
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", //true or false
 AUTO_REPLY:process.env.AUTO_REPLY || "true", //true or false
